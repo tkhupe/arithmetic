f94dfe6321844be93b62e58552dfb76abb00a7d6
Tshakalisa Khupe <88752230+tkhupe@users.noreply.github.com>
2023-02-23 05:25:23 -0600

    added functions

---
+++ arithmetic.js
@@ -0,0 +1,28 @@
+// Fill in the body of each function with the code required
+
+var add = function(num1, num2) {
+    for (let i = num1; i <= num2; i++) {
+        return num1 + num2
+    }
+};
+
+var subtract = function(num1, num2) {
+    for (let i = num1; i => num2; i--) {
+        return num1 - num2
+    }
+
+};
+
+var multiply = function(num1, num2) {
+    for (let i = num1; i <= num2; i++) {
+        return num1 * num2
+    }
+
+};
+
+var divide = function(num1, num2) {
+    for (let i = num1; i => num2; i++) {
+        return num1 / num2
+    }
+
+};
